d1d10352584d4707d320fa8eac1c52136e3f2b84
Paul ALNET <paul@shimaore.net>
2022-01-24 09:11:18 +0100

    Create code structure for vim shortcut script

---
+++ macroScript.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Should be provided/are needed :
+# - The source file
+# - The custom lib directory
+# - The target directory (create if needed)
+# - The archive directory 
+
+
+# Check if parameters are correct
+
+
+# Clear target directory
+
+
+# Copy source file and tests
+
+
+# Inject custom libs, fail if can't be found
+
+
+# Copy file to archive dir. Date file
+
+
+# Copy file to "latest" directory (for easy access)
+
+
+# Plug tests on stdin, compare stdout with expexted results
+
