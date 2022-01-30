8161f65029eb6f1340e618444ad5881dddf165a5
Paul ALNET <paul@shimaore.net>
2022-01-30 15:32:02 +0100

    Added script to upload target file

---
+++ macroUpload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Needed :
+# - PHP session ID : $CODINSA_PHPSESS
+# - submission token (can be found in submission page form) : $CODINSA_SUBTOKEN
+# - project root directory : CODINSA_ROOTDIR
+
+curl -v -X POST -H "Cookie: PHPSESSID=$CODINSA_PHPSESS" \
+  --data-urlencode "form%5Blanguage%5D=14" \
+  --data-urlencode "form%5B_token%5D=$CODINSA_SUBTOKEN" \
+  --data-urlencode "form%5Bcode%5D@$CODINSA_ROOTDIR/target/target.js" \
+  https://training.codinsa.org/qualifications/code/202
+
